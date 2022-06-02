586fd75189a941756837630555ae9c99d6d16fab
Alon Sardas <alonsardas@gmail.com>
2022-06-03 01:18:49 +0300

    Adding some sufaces for example

--- main.py
+++ main.py
@@ -3,173 +3,66 @@ import matplotlib.pyplot as plt
 import matplotlib.widgets
 import numpy as np
 
-
-class SimpleMiuraOri(object):
-    def __init__(self, ls_x, ls_y, angle=np.pi / 4):
-        self.ls_x = ls_x
-        self.ls_y = ls_y
-        self.angle = angle
-
-        self.num_of_panels = len(ls_x) * len(ls_y)
-        self.rows = len(ls_y)
-        self.columns = len(ls_x)
-        self._d_rows = self.rows + 1
-        self._d_cols = self.columns + 1
-
-        # This array contains 1 dot per panel (the bottom left for example)
-        # with 3 coordinates
-        self.initial_dots = np.zeros((3, self._d_rows * self._d_cols))
-        self._indexes = np.arange(self._d_rows * self._d_cols).reshape((self._d_rows, self._d_cols))
-        self._build_dots()
-        self.dots = self.initial_dots.copy()
-
-    def _build_dots(self):
-        dxs = np.append(0, self.ls_x)
-        xs = np.cumsum(dxs)
-        xs = xs.astype('float64')
-        xs *= np.cos(self.angle)
-        for r in range(self._d_rows):
-            self.initial_dots[0, self._indexes[r, :]] = xs
-
-        ys = np.append(0, np.cumsum(self.ls_y))
-        ys = ys.astype('float64')
-        sign = +1
-        for c in range(self._d_cols):
-            ys += sign * dxs[c] * np.sin(self.angle)
-            self.initial_dots[1, self._indexes[:, c]] = ys
-            sign *= -1
-
-    def plot(self, ax: matplotlib.axes.Axes, should_rotate=True):
-        # print('zs: {}'.format(dots[2, :].reshape(self._d_rows, self._d_cols)))
-
-        # ax.scatter3D(self.dots[0, :], self.dots[1, :], self.dots[2, :])
-        # ax.scatter3D(self.dots[0, 4:], self.dots[1, 4:], self.dots[2, 4:])
-
-        plot_dots = self.dots.copy()
-        if should_rotate:
-            R_xy = np.array([
-                [np.cos(self.angle), -np.sin(self.angle), 0],
-                [np.sin(self.angle), np.cos(self.angle), 0],
-                [0, 0, 1]])
-            plot_dots = R_xy.transpose() @ plot_dots
-
-        ax.plot_surface(plot_dots[0, :].reshape((self._d_rows, self._d_cols)),
-                        plot_dots[1, :].reshape((self._d_rows, self._d_cols)),
-                        plot_dots[2, :].reshape((self._d_rows, self._d_cols)))
-
-    def set_omega(self, omega):
-        self.dots = self.initial_dots.copy()
-        dots = self.dots
-
-        sigma = 1
-        omega1 = omega
-        omega2 = self._calc_omega2(sigma, omega1)
-
-        c1 = np.cos(omega1)
-        s1 = np.sin(omega1)
-
-        R1 = np.array([
-            [c1, 0, s1],
-            [0, 1, 0],
-            [-s1, 0, c1]])
-        R2 = self._create_R2(omega2)
-
-        start_sign_x = 1
-        start_sign_y = -1
-
-        R2_alternating = R2.copy()
-        if start_sign_y == -1:
-            R2_alternating = R2_alternating.transpose()
-
-        # The first row is done externally, beause we don't change signs there
-        R1_alternating = R1.copy()
-        if start_sign_x == -1:
-            R1_alternating = R1_alternating.transpose()
-        # """
-        for c in range(self.columns, 1, -1):
-            dots_indices = self._indexes[-1:, c:].flat
-            # print('Changing manually cols indices {}'.format(np.array(dots_indices)))
-
-            base_x = dots[0, self._indexes[-1, c - 1]]
-            dots[0, dots_indices] -= base_x
-            dots[:, dots_indices] = R1_alternating @ dots[:, dots_indices]
-            dots[0, dots_indices] += base_x
-            R1_alternating = R1_alternating.transpose()
-
-        for r in range(self.rows - 1, -1, -1):
-            R1_alternating = R1.copy()
-            if start_sign_x == -1:
-                R1_alternating = R1_alternating.transpose()
-            start_sign_x *= -1
-
-            for c in range(self.columns, 1, -1):
-                dots_indices = self._indexes[r, c:]
-                # print('Changing cols indices {}'.format(np.array(dots_indices)))
-
-                base_x = dots[0, self._indexes[r, c - 1]]
-                dots[0, dots_indices] -= base_x
-                dots[:, dots_indices] = R1_alternating @ dots[:, dots_indices]
-                dots[0, dots_indices] += base_x
-                R1_alternating = R1_alternating.transpose()
-            # """
-            # """
-            if r == 0:
-                # It is "ok" but not necessary ro fold the last row, since
-                # it translates to a rigid rotation
-                R2_alternating = self._create_R2(+omega2 / 2)
-                # R2_alternating = R2_alternating.transpose()
-
-            dots_indices = self._indexes[r:, :].flat
-            # print('Changing row indices {}'.format(np.array(dots_indices)))
-            base_y = dots[1, self._indexes[r, 0]]
-            dots[1, dots_indices] -= base_y
-            dots[:, dots_indices] = R2_alternating @ dots[:, dots_indices]
-            dots[1, dots_indices] += base_y
-
-            R2_alternating = R2_alternating.transpose()
-
-    def _create_R2(self, omega2):
-        c2 = np.cos(omega2)
-        s2 = np.sin(omega2)
-
-        R2_t = np.array([
-            [c2, 0, s2],
-            [0, 1, 0],
-            [-s2, 0, c2]])
-        R_xy = np.array([
-            [np.cos(self.angle), -np.sin(self.angle), 0],
-            [np.sin(self.angle), np.cos(self.angle), 0],
-            [0, 0, 1]])
-        R2 = R_xy @ R2_t @ R_xy.transpose()
-        return R2
-
-    def _calc_omega2(self, s, o):
-        a = self.angle
-        b = np.pi - a
-        cos_a = np.cos(a)
-        sin_a = np.sin(a)
-        cos_b = np.cos(b)
-        sin_b = np.sin(b)
-        nom = (-s + cos_a * cos_b) * np.cos(o) + sin_a * sin_b
-        deno = -s + cos_a * cos_b + sin_a * sin_b * np.cos(o)
-        return np.arccos(nom / deno)
+from miuraori import SimpleMiuraOri
 
 
 def create_cylinder():
-    xs = 10
-    ys = 10
-    ls_x = np.ones(xs)
-    ls_y = np.ones(ys)
-    # ls_y[::2] += np.arange(5)/2
-    ls_y[::2] += np.arange(ys / 2) ** 2 / 100
-    origami = SimpleMiuraOri(ls_x, ls_y)
+    ls_y = create_circular_ls()
+    print(ls_y)
+    origami = SimpleMiuraOri(np.ones(30) * 0.1, ls_y)
     return origami
+
+
+def create_sphere():
+    ls_y = create_circular_ls()
+    # ls_x = np.append(ls_y[1:], ls_y[0])
+    origami = SimpleMiuraOri(np.ones(20) * 0.1, ls_y)
+    return origami
+
+
+def create_sphere_with_lag():
+    ls_y = create_circular_ls()
+    # ls_x = np.append(ls_y[1:], ls_y[0])
+    origami = SimpleMiuraOri(np.ones(20) * 0.1, np.append(np.ones(20)*0.1, ls_y))
+    return origami
+
+
+def create_saddle():
+    ls = create_circular_ls()
+    origami = SimpleMiuraOri(ls, ls)
+    return origami
+
+
+def create_circular_ls(num_of_angles=20):
+    base_angle = np.pi / 8
+    tan = np.tan(base_angle)
+
+    angles = np.linspace(np.pi + np.pi / 4, np.pi + 3 * np.pi / 4, num_of_angles)
+    xs = np.cos(angles)
+    ys = np.sin(angles)
+
+    ls = np.zeros((len(angles) - 1) * 2)
+    for i in range(len(angles) - 1):
+        a_x = xs[i]
+        a_y = ys[i]
+        b_x = xs[i + 1]
+        b_y = ys[i + 1]
+
+        middle_x = 1 / (2 * tan) * (b_y - a_y + (a_x + b_x) * tan)
+        middle_y = 1 / 2 * (b_y + a_y + (b_x - a_x) * tan)
+
+        d1 = np.sqrt((a_x - middle_x) ** 2 + (a_y - middle_y) ** 2)
+        d2 = np.sqrt((b_x - middle_x) ** 2 + (b_y - middle_y) ** 2)
+        ls[2 * i] = d1
+        ls[2 * i + 1] = d2
+
+    return ls
 
 
 def main():
     # TODO: the angle parameter doesn't seem to work...
-    origami = SimpleMiuraOri([1, 2, 3, 1, 1, 1, 1, 1], [1, 3, 1, 1, 1, 1, 1], angle=np.pi / 10)
-    # origami = create_cylinder()
+
+    # origami = SimpleMiuraOri([1, 2, 3, 1, 1, 1, 1, 1], [1, 3, 1, 1, 1, 1, 1], angle=np.pi / 10)
     # origami = SimpleMiuraOri([1, 2, 3, 1], [1, 3, 1])
     # origami = SimpleMiuraOri([1, 1, 1, 1], [1])
     # origami = SimpleMiuraOri([1], [1, 1, 1, 1])
@@ -178,6 +71,12 @@ def main():
     # origami = SimpleMiuraOri([1, 1], [1, 1])
     # origami.set_omega(np.pi/40)
     # origami.set_omega(np.pi / 4)
+
+
+    # origami = create_cylinder()
+    # origami = create_saddle()
+    origami = create_sphere_with_lag()
+    # origami = create_sphere()
 
     fig = plt.figure()
 
@@ -200,6 +99,7 @@ def main():
         ax.clear()
         origami.set_omega(omega)
         origami.plot(ax)
+        # ax.set_autoscale_on(False)
         ax.set_xlim([0, lim])
         ax.set_ylim([0, lim])
         ax.set_zlim([-lim / 2, lim / 2])
